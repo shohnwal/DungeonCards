c4003d7f824b5cfcfbf5a5acec010551045f5d54
Stefan Hohnwald <kyoai@freenet.de>
2018-03-20 11:25:04 +0100

    ue4 init

---
+++ Source/DungeonCards/DungeonCards.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "DungeonCards.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DungeonCards, "DungeonCards" );
+
+DEFINE_LOG_CATEGORY(LogDungeonCards)
+ 
---
+++ Source/DungeonCards/DungeonCardsGameMode.cpp
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "DungeonCardsGameMode.h"
+#include "DungeonCardsPlayerController.h"
+#include "DungeonCardsCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ADungeonCardsGameMode::ADungeonCardsGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = ADungeonCardsPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
